61442b34c09d17b87e7cf3c839f3adea440cfbaa
AseelHamamreh <aseel.hamamreh@hotmail.com>
2025-10-31 15:32:22 +0100

    basic tests done

---
+++ src/main/java/com/example/ElectronicsProduct.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class ElectronicsProduct extends Product implements Shippable {
+    private int warrantyMonths;
+    private BigDecimal weight;
+
+    public ElectronicsProduct(UUID id, String name, Category category, BigDecimal price,
+                              int warrantyMonths, BigDecimal weight) {
+        super(id, name, category, price);
+        if (warrantyMonths < 0) {
+            throw new IllegalArgumentException("Warranty months cannot be negative.");
+        }
+        this.warrantyMonths = warrantyMonths;
+        this.weight = weight;
+    }
+
+    @Override
+    public String productDetails() {
+        return "Electronics: " + name() + ", Warranty: " + warrantyMonths + " months";
+    }
+
+    @Override
+    public BigDecimal calculateShippingCost() {
+        if (weight.doubleValue() > 5.0) {
+            return new BigDecimal("128");
+        } else {
+            return new BigDecimal("79");
+        }
+    }
+
+    @Override
+    public double weight() {
+        return weight.doubleValue();
+    }
+}
